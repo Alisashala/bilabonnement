b000dcfad1552b06595d0993f15153ae95a5a02a
Rxyxna <Rayanabaga@outlook.com>
2023-12-07 13:53:51 +0100

    minor changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import DeleteCustomer from './components/DeleteCustomer';
 import EditCustomer from './components/EditCustomer';
 import CustomerDetails from './components/CustomerDetails';
 import Frontpage from './components/Frontpage';
+import SupportAndHelp from './components/SupportAndHelp';
+import UpdatesAndNews from './components/UpdatesAndNews';
 
 function App() {
   return ( 
@@ -24,6 +26,10 @@ function App() {
         <Route path="/returns" element={<div>Returns Page</div>} />
         {/* Adjust the following route based on your requirements */}
         <Route path="/return/:id" element={<div>Return Details Page</div>} />
+
+        <Route path="/support-and-help" element={<div>Support And Help</div>} />
+        <Route path="/updates-and-news" element={<div>Updates And News</div>} />
+
       </Routes>
     </Router>
   );
